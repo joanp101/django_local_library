710f77a3e4182d0935512da3d3788fea7de7edd2
Joan Pellegrino <joanpellegrino04@gmail.com>
2020-05-10 09:50:23 -0500

    scretekey

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get ('DJANGO_SECRET_KEY', 'cg # p $ g + j9tax! # a3cup @ 1 $ 8obt2 _ + & k3q + pmu) 5% asj6yjpkag')
+#SECRET_KEY = os.environ.get ('DJANGO_SECRET_KEY', 'cg # p $ g + j9tax! # a3cup @ 1 $ 8obt2 _ + & k3q + pmu) 5% asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = False
