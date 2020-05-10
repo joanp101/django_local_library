d259d400c2fda9a47788332d3aa3a45dcdb416de
Joan Pellegrino <joanpellegrino04@gmail.com>
2020-05-10 02:45:15 -0500

    settings

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get ('DJANGO_SECRET_KEY', 'cg # p $ g + j9tax! # a3cup @
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = False
 DEBUG = os.environ.get ('DJANGO_DEBUG', '') != 'False'  
-ALLOWED_HOSTS = ['https://shrouded-stream-38586.herokuapp.com/']
+ALLOWED_HOSTS = ['https://shrouded-stream-38586.herokuapp.com/' '127.0.0.1' ]
 
 
 # Application definition
